bfb53da3360e194191058b16801d978943188d89
Justin Yee <justinyee1125@gmail.com>
2024-12-24 18:53:37 -0700

    formatted add contact screen

--- frontend/src/components/AddContactScreen.tsx
+++ frontend/src/components/AddContactScreen.tsx
@@ -2,8 +2,10 @@ import Column from './Column';
 
 function AddContactsScreen() {
     return <Column id="" className="chat-history-col flex-grow-1 flex-shrink-1" style={{}}>
-        <input type="text" id="name-text-input-add-contact" className="text-input-sign-up-login" placeholder="contact name .."/>
-        <input type="text" id="phone-num-text-input-add-contact" className="text-input-sign-up-login" placeholder="phone number .."/>
+        <p className="add-contact-header"> Add Contact </p>
+        <input type="text" id="" className="text-input-sign-up-login name-text-input-add-contact" placeholder="contact name .."/>
+        <input type="text" id="" className="text-input-sign-up-login phone-num-text-input-add-contact" placeholder="phone number .."/>
+        <br/>
         <button type="button" className="confirm-add-contact-btn btn btn-success"> Add Contact </button>
     </Column>
 }
